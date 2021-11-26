14f7d8ff13a12d8e81bd731fbf937f1a6361d1e4
AJDaouda <AJ.Daouda@outlook.com>
2021-11-25 23:50:24 -0800

    QuizApp_Version1.2

--- app/src/main/java/com/example/quizapplication/MainActivity.java
+++ app/src/main/java/com/example/quizapplication/MainActivity.java
@@ -10,33 +10,38 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.quizapplication.Model.MyApp;
 import com.example.quizapplication.Model.QuestionManager;
 
 public class MainActivity extends AppCompatActivity {
-    FragmentManager fm = getSupportFragmentManager();
-    //QuestionMananger qM = new QuestionMananger90
 
-    QuestionManager qM = ((MyApp) getApplication()).getqManager();
+    Button btn_true, btn_false;
+    ProgressBar myProgress;
 
-    public void updateFragment(int qId, int colorId){
-        QuestionFragment qfragment = (QuestionFragment) fm.findFragmentById(R.id.frag_container);
-        if (qfragment == null){
-            // add f3
-            qfragment = new QuestionFragment();
-            fm.beginTransaction().add(R.id.frag_container,qfragment).commit();
-            //fm.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            }
-    }
+    //QuestionManager qM = ((MyApp) getApplication()).getqManager();
+    QuestionManager qM = new QuestionManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_true = (Button) findViewById(R.id.btn_T);
+        btn_false = (Button) findViewById(R.id.btn_F);
+        myProgress =  (ProgressBar) findViewById(R.id.pbar);
         updateFragment(qM.getQuestionBank().get(0).getQuestionId(),qM.getColors().get(0));
     }
+
+    public void updateFragment(int qId, int colorId){
+        FragmentManager fm = getSupportFragmentManager();
+        fm.findFragmentById(R.id.main_frame_id);
+        QuestionFragment qfragment = QuestionFragment.newInstance(qId,colorId);
+        fm.beginTransaction().add(R.id.main_frame_id,qfragment).commit(); }
 
     // Showing the "top side menu" in the main activity
     @Override
